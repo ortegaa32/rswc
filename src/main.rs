0648df948e0c6246f0a59071399030c62d262bc1
Arturo Ortega <arturo6111@ahoo.com>
2024-09-02 15:34:50 -0700

    Implement basic cli tool using flags

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(short='c')]
+    bytes: bool,
+    #[arg(short='w')]
+    words: bool,
+    #[arg(short='l')]
+    lines: bool,
+    #[arg(short='m')]
+    multibytes: bool,
+    path: std::path::PathBuf
+}
+
 fn main() {
+    let args = Cli::parse();
     println!("Hello, world!");
+    println!("Args: {:?}", args);
 }
